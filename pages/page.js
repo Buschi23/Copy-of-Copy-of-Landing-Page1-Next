4c2bd0425b96081b4c69af32714e4284ed0673be
Buschi23 <107368011+Buschi23@users.noreply.github.com>
2022-06-23 11:48:30 +0200

    Update page.js

--- pages/page.js
+++ pages/page.js
@@ -5,6 +5,7 @@ const Page = (props) => {
   return (
     <>
       <link rel="canonical" href="https://reparaturservice-kormann.de/index">
+      </link>
       <div className="page-container">
         <Head>
           <title>Page - Copy of Copy of Landing Page1</title>
